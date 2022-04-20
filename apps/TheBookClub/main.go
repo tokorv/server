83797595e9787f21dbadb4a81f5c18e2948f6d9f
William Burton <twitchifygamers@gmail.com>
2022-04-20 12:18:07 +0000

    its lunch time

--- apps/TheBookClub/main.go
+++ apps/TheBookClub/main.go
@@ -1,1 +1,7 @@
 package TheBookClub
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/amber"
+)
+
--- server.go
+++ server.go
@@ -19,7 +19,5 @@ func main() {
 		})
 	})
 	
-	
-	
 	server.Listen(":8080")
 }
